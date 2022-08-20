eb71165da0c74db5a7142e25010faf0598ae0889
kelvin <kevomusa6565@gmail.com>
2022-08-20 23:16:54 +0300

    set up app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
 import { Component } from '@angular/core';
+import * as RecordRTC from 'recordrtc';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'wav-voice';
+
+  //Lets initiate Record OBJ
+   public record: any;
+   //Will use this flag for detect recording
+   public recording = false;
+   //Url of Blob
+   public url: any;
+   public error: any;
+   constructor(private domSanitizer: DomSanitizer) {
+   }
+   sanitize(url:string){
+       return this.domSanitizer.bypassSecurityTrustUrl(url);
+   }
+   /**
+    * Start recording.
+    */
+   initiateRecording() {
+
+       this.recording = true;
+       let mediaConstraints = {
+           video: false,
+           audio: true
+       };
+       navigator.mediaDevices
+           .getUserMedia(mediaConstraints)
+           .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+   }
+   /**
+    * Will be called automatically.
+    */
+   successCallback(stream: any) {
+       var options = {
+           mimeType: "audio/wav",
+           numberOfAudioChannels: 1
+       };
+       //Start Actuall Recording
+       var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
+       this.record = new StereoAudioRecorder(stream, options);
+       this.record.record();
+   }
+   /**
+    * Stop recording.
+    */
+   stopRecording() {
+       this.recording = false;
+       this.record.stop(this.processRecording.bind(this));
+   }
+   /**
+    * processRecording Do what ever you want with blob
+    * @param  {any} blob Blog
+    */
+   processRecording(blob: any) {
+       this.url = URL.createObjectURL(blob);
+   }
+   /**
+    * Process Error.
+    */
+   errorCallback(error: any) {
+       this.error = 'Can not play audio in your browser';
+   }
 }
